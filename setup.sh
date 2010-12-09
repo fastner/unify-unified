f2a13d088164cd73f7feb5cffb039ef6036c260d
Sebastian Fastner <sf@fastnerit.de>
2010-12-09 18:26:21 +0100

    Add version check of python and better messages

--- setup.sh
+++ setup.sh
@@ -2,17 +2,32 @@
 
 echo "Unify unified workspace setup tool"
 echo "Copyright 2010 Sebastian Fastner"
-echo ""
+echo
 echo "visit http://www.unify-training.com"
-echo ""
+echo
 
+echo "---- Please wait, initialisation of submodules. This may take some time ----"
 if [ -d unify/framework/source/class/unify ]; then
   echo "-> Unify initialisation is already done"
 else
-  echo "---- Please wait, initialisation of submodules. This may take some time ----"
+  echo
   git submodule update --init --recursive
+  echo
   echo "---- Done, thank you for waiting. Have fun with unify ----------------------"
 fi
 
-echo ""
+PYTHON3_EXEC=`which python3`
+if [ "$PYTHON3_EXEC" = "" ] ; then
+  echo
+  echo "Error: Please install python 3"
+  exit 1
+else
+  $PYTHON3_EXEC -c 'import platform; exit(0) if int(platform.python_version().split(".")[0]) >= 3 else exit(1);'
+  if [ $? -eq 1 ]; then
+    echo "Error: Please install python version 3"
+    exit 1
+  fi
+fi
+
+echo
 echo "Create your application with 'unify/tool/create-application.py -n <namespace>'"
